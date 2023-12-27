9ec162daf0fab99d29365904b892e70bc0ccfc10
Vansh16aug <kumarvansh16aug@gmail.com>
2023-12-27 21:17:25 +0530

    2

--- patterns/pt4.java
+++ patterns/pt4.java
@@ -1,14 +1,15 @@
+package patterns;
 /*
     *****
-    *****
-    *****
-    *****
+    ****
+    ***
+    **
+    *
  */
-public class pt1 {
+public class pt4 {
     public static void main(String args[]){
-        for(int i=1;i<=5;i++){
-            // System.out.println("*");
-            for(int j=1;j<=5;j++){
+        for(int i=5;i>=1;i--){
+            for(int j=1;j<=i;j++){
                 System.out.print("*");
             }
             System.out.println();
---
+++ patterns/pt5.java
@@ -0,0 +1,8 @@
+package patterns;
+public class pt5 {
+    public static void main(String args[]){
+        for(int i=1;i<=5;i++){
+            for()
+        }
+    }
+}
