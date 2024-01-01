6851b6b0c16330f8e7419ac8240b9015ca8364e0
Vansh16aug <kumarvansh16aug@gmail.com>
2024-01-01 14:51:52 +0530

    Another day

--- patterns/pt5.java
+++ patterns/pt5.java
@@ -8,8 +8,14 @@
 package patterns;
 public class pt5 {
     public static void main(String args[]){
-        for(int i=1;i<=5;i++){
-            for()
+        int n=4;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
         }
     }
 }
