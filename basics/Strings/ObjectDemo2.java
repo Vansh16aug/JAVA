c61102a3d6ab6e304895e263a8cb56c6dce1439c
Vansh16aug <kumarvansh16aug@gmail.com>
2024-03-01 18:08:51 +0530

    oops

---
+++ basics/Strings/ObjectDemo2.java
@@ -0,0 +1,42 @@
+package basics.Strings;
+import java.util.Objects;
+
+class Teacher{
+    String name;
+    int id;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Teacher teacher = (Teacher) o;
+        return id == teacher.id && Objects.equals(name, teacher.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, id);
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                '}';
+    }
+}
+public class ObjectDemo2 {
+    public static void main(String a[]){
+        Teacher obj = new Teacher();
+        obj.id = 1;
+        obj.name = "ABC";
+
+        Teacher obj1 = new Teacher();
+        obj1.id = 1;
+        obj1.name = "ABC";
+
+        System.out.println(obj.toString());
+        System.out.println(obj.equals(obj1));
+    }
+}
